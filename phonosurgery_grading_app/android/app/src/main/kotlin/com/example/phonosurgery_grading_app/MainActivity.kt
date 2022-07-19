5cbf1823a06fbec62a86079ac3d6bc086e68e64c
Matthias Kim <matthiastk805@gmail.com>
2022-07-19 17:28:01 -0400

    Tiles ready, screen can change

---
+++ phonosurgery_grading_app/android/app/src/main/kotlin/com/example/phonosurgery_grading_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.phonosurgery_grading_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ phonosurgery_grading_app/lib/screens/gelinjection.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Videos extends StatelessWidget {
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text(''),
+          centerTitle: true,
+        ),
+        /*body: Center(
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.all(20),
+            ),
+            child: const Text('Last Screen'),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomeScreen(),
+                  ));
+            },
+          ),
+        ),*/
+      );
+}
