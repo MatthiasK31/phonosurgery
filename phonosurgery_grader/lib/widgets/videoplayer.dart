7388fec7ac0e3a78049a528a14c001921e39542c
Matthias Kim <matthiastk805@gmail.com>
2022-09-09 22:02:16 -0400

    need to work on table

--- phonosurgery_grader/lib/widgets/videoplayer.dart
+++ phonosurgery_grader/lib/widgets/videoplayer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
+import 'package:video_player/video_player.dart'; 
 /* import 'package:chewie/chewie.dart';
 
 class Vid extends StatefulWidget {
